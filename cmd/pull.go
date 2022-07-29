64f24d72339774d68ffc84f223aacc71d3113267
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2022-07-29 19:56:25 +0530

    intilzation done and pull command written

---
+++ cmd/pull.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// pullCmd represents the pull command
+var pullCmd = &cobra.Command{
+	Use:   "pull",
+	Short: "Take pull from repo",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("pull called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(pullCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// pullCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// pullCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
